e28e79aa40e3ebb54b8325439778c3c26bc0041d
Aunmag <aunmag@gmail.com>
2019-09-28 16:28:56 +0500

    Черновой вариант авторизации

--- app/controllers/Controller.java
+++ app/controllers/Controller.java
@@ -7,6 +7,10 @@ import play.mvc.Result;
 
 public abstract class Controller extends play.mvc.Controller {
 
+    public static String SESSION_VALUE_TRUE = "1";
+    public static String SESSION_VALUE_FALSE = "0";
+    public static String SESSION_KEY_IS_ADMIN = "is_admin";
+
     public String m(String key, Object... parameters) {
         return getMessages().at(key, parameters);
     }
@@ -17,6 +21,10 @@ public abstract class Controller extends play.mvc.Controller {
 
     public Messages getMessages() {
         return Http.Context.current().messages();
+    }
+
+    protected boolean isAdmin() {
+        return SESSION_VALUE_TRUE.equals(session(SESSION_KEY_IS_ADMIN));
     }
 
 }
--- app/controllers/api/v1/ApiController.java
+++ app/controllers/api/v1/ApiController.java
@@ -1,5 +1,6 @@
 package controllers.api.v1;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import controllers.Controller;
 import play.mvc.Result;
 
@@ -7,6 +8,20 @@ public class ApiController extends Controller {
 
     public Result setLanguage(String language) {
         ctx().changeLang(language);
+        return ok();
+    }
+
+    public Result access() {
+        return ok(new ObjectMapper().createObjectNode().put("is_admin", isAdmin()));
+    }
+
+    public Result loginAsUser() {
+        session(SESSION_KEY_IS_ADMIN, SESSION_VALUE_FALSE);
+        return ok();
+    }
+
+    public Result loginAsAdmin() {
+        session(SESSION_KEY_IS_ADMIN, SESSION_VALUE_TRUE);
         return ok();
     }
 
