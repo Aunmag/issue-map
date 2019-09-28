d8394ea014cdbf5a4e872b561376d0b98abec600
Vasya-secureCode <55871754+Vasya-secureCode@users.noreply.github.com>
2019-09-28 21:47:58 +0500

    Поправил таблицу и тестовые данные (#10)

--- app/models/Authority.java
+++ app/models/Authority.java
@@ -12,16 +12,38 @@ public class Authority extends BaseModel {
 
     public static final Finder<Integer, Authority> FIND = new Finder<>(Authority.class);
 
-    @Column(nullable = false, length = 100)
+    @Column(length = 100)
     public String name;
 
-    public Authority(String name) {
+    @Column(length = 100)
+    public String phone;
+
+    @Column(length = 100)
+    public String email;
+
+    @Column(length = 100)
+    public String url;
+
+    @Column(length = 100)
+    public String address;
+
+
+    public Authority(String name, String phone, String email, String url, String address) {
         this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.url = url;
+        this.address = address;
     }
 
     @Override
     public ObjectNode toJson(Messages messages) {
-        return toJsonId().put("name", name);
+        return toJsonId()
+            .put("name", name)
+            .put("phone", phone)
+            .put("email", email)
+            .put("url", url)
+            .put("address", address);
     }
 
 }
